d2bbf61c3f076da495617abcbac22fe206bedd4d
eloisemaun <eloise@maun.me>
2019-07-30 09:24:34 -0400

    Build + yaml routing for build

---
+++ build/precache-manifest.d6c4108a132cbb0bd480010de5aabb01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce22bc54fa233ce1a72500691ed5b6bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66e71d50af97952bb97b",
+    "url": "/static/css/main.c558eecb.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "66e71d50af97952bb97b",
+    "url": "/static/js/main.66906a4c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
